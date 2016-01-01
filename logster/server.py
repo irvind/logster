3b5e057fc5bbb963605f127ea3a982c42cb9a110
Eugene Obukhov <irvind25@gmail.com>
2016-01-01 23:19:21 +0300

    Connect to the database and make it available in handlers

--- logster/server.py
+++ logster/server.py
@@ -4,6 +4,8 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application
 from tornado.httpserver import HTTPServer
 
+from motor.motor_tornado import MotorClient
+
 from . import handlers
 from .conf import config
 
@@ -19,10 +21,34 @@ class LogsterApplication(Application):
     }
 
     def __init__(self):
+        db = self._connect_to_db()
+
         super(LogsterApplication, self).__init__(
             handlers=self.handlers,
+            db=db,
             **self.settings
         )
+
+    def _connect_to_db(self):
+        db_conf = config['db']
+
+        with_auth = db_conf.get('user') and db_conf.get('password')
+        if with_auth:
+            auth = '{user}:{password}@'.format(**db_conf)
+            if db_conf.get('authDbName'):
+                auth_db = '/' + db_conf['authDbName']
+        else:
+            auth = ''
+            auth_db = ''
+
+        dest = '{host}:{port}'.format(**db_conf)
+
+        conn_str = 'mongodb://' + auth + dest + auth_db
+        self.db_client = MotorClient(conn_str)
+
+        db = getattr(self.db_client, db_conf['dbName'])
+
+        return db
 
 
 def run_server():
